579f0328562557d7c0b6f3c5b27bdff36fa38386
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-08-29 22:01:52 +0900

    setting authSlice

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -1,1 +1,76 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {  createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+import axios from 'axios'
+import { PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from "../types"
+
+const apuUrl = process.env.REACT_APP_DEV_API_URL;
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    openSignIn: true,
+    openSignUp: false,
+    openProfile: false,
+    isLoadingAuth: false,
+    myprofile: {
+      id: 0,
+      nickName: "",
+      userProfile: 0,
+      created_on: "",
+      img: "",
+    },
+    profiles: [
+      {
+        id: 0,
+        nickName: "",
+        userProfile: 0,
+        created_on: "",
+        img: "",
+      }
+    ]
+  },
+  reducers: {
+    // dispatch経由で呼び出すことによって自由自在に値を変更することができる
+    fetchCredStart(state) {
+      state.isLoadingAuth = true;
+    },
+    fetchCredEnd(state) {
+      state.isLoadingAuth = false;
+    },
+    setOpenSignIn(state) {
+      state.openSignIn = true;
+    },
+    resetOpenSignIn(state) {
+      state.openSignIn = false;
+    },
+    setOpenSignUp(state) {
+      state.openSignUp = true;
+    },
+    resetOpenSignUp(state) {
+      state.openSignUp = false;
+    },
+    setOpenProfile(state) {
+      state.openProfile = true;
+    },
+    resetOpenProfile(state) {
+      state.openProfile = false;
+    },
+    editNickname(state, action) {
+      state.myprofile.nickName = action.payload;
+    },
+  },
+});
+
+export const {
+  fetchCredStart,
+  fetchCredEnd,
+  setOpenSignIn,
+  resetOpenSignIn,
+  setOpenSignUp,
+  resetOpenSignUp,
+  setOpenProfile,
+  resetOpenProfile,
+  editNickname,
+} = authSlice.actions;
+export const selectCount = (state: RootState) => state.counter.value;
+export default authSlice.reducer;
