39ea740701b16549a86963484bf68f06064de4cb
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-08-30 17:16:30 +0900

    postSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,14 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 // カスタムスライスをimport
 import authReducer from '../features/auth/authSlice';
+import postReducer from '../features/post/postSlice';
 
 // sliceはこちらで登録する必要がある
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     auth: authReducer,
+    post: postReducer,
   },
 });
 
---
+++ src/features/post/postSlice.ts
@@ -0,0 +1,200 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import axios from "axios";
+// 投稿に関する型定義
+import { PROPS_NEWPOST, PROPS_LIKED, PROPS_COMMENT } from "../types";
+
+const apiUrlPost = `${process.env.REACT_APP_DEV_API_URL}api/post/`;
+const apiUrlComment = `${process.env.REACT_APP_DEV_API_URL}api/comment/`;
+
+// 非同期処理を同期的に実行するための実装
+// 投稿一覧を取得する
+export const fetchAsyncGetPosts = createAsyncThunk("post/get", async () => {
+  const res = await axios.get(apiUrlPost, {
+    headers: {
+      Authorization: `JWT ${localStorage.localJWT}`,
+    },
+  });
+  return res.data;
+});
+
+// 新しい投稿を実施する
+export const fetchAsyncNewPost = createAsyncThunk(
+  "post/post",
+  async (newPost: PROPS_NEWPOST) => {
+    const uploadData = new FormData();
+    uploadData.append("title", newPost.title);
+    newPost.img && uploadData.append("img", newPost.img, newPost.img.name);
+    const res = await axios.post(apiUrlPost, uploadData, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+// いいねを実施する
+export const fetchAsyncPatchLiked = createAsyncThunk(
+  "post/patch",
+  async (liked: PROPS_LIKED) => {
+    const currentLiked = liked.current;
+    const uploadData = new FormData();
+
+    let isOverlapped = false;
+    currentLiked.forEach((current) => {
+      if (current === liked.new) {
+        isOverlapped = true;
+      } else {
+        uploadData.append("liked", String(current));
+      }
+    });
+
+    if (!isOverlapped) {
+      uploadData.append("liked", String(liked.new));
+    } else if (currentLiked.length === 1) {
+      uploadData.append("title", liked.title);
+      const res = await axios.put(`${apiUrlPost}${liked.id}/`, uploadData, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `JWT ${localStorage.localJWT}`,
+        },
+      });
+      return res.data;
+    }
+    const res = await axios.patch(`${apiUrlPost}${liked.id}/`, uploadData, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+// コメント一覧を取得する
+export const fetchAsyncGetComments = createAsyncThunk(
+  "comment/get",
+  async () => {
+    const res = await axios.get(apiUrlComment, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+// コメントを投稿する
+export const fetchAsyncPostComment = createAsyncThunk(
+  "comment/post",
+  async (comment: PROPS_COMMENT) => {
+    const res = await axios.post(apiUrlComment, comment, {
+      headers: {
+        Authorization: `JWT ${localStorage.localJWT}`,
+      },
+    });
+    return res.data;
+  }
+);
+
+// sliceの中のstateを定義
+export const postSlice = createSlice({
+  // sliceの名前の定義
+  name: "post",
+  // 初期値
+  initialState: {
+    isLoadingPost: false,
+    openNewPost: false,
+    posts: [
+      {
+        id: 0,
+        title: "",
+        userPost: 0,
+        created_on: "",
+        img: "",
+        liked: [0],
+      },
+    ],
+    comments: [
+      {
+        id: 0,
+        text: "",
+        userComment: 0,
+        post: 0,
+      },
+    ],
+  },
+  // dispatchで実行する関数
+  reducers: {
+    // 投稿中のステータス
+    fetchPostStart(state) {
+      state.isLoadingPost = true;
+    },
+    // 投稿完了のステータス
+    fetchPostEnd(state) {
+      state.isLoadingPost = false;
+    },
+    // 新しく投稿するステータス
+    setOpenNewPost(state) {
+      state.openNewPost = true;
+    },
+    // 投稿完了のステータス
+    resetOpenNewPost(state) {
+      state.openNewPost = false;
+    },
+  },
+  // dispatchの実行状態による実行内容、完了後、失敗した時、実行中
+  extraReducers: (builder) => {
+    builder.addCase(fetchAsyncGetPosts.fulfilled, (state, action) => {
+      return {
+        ...state,
+        posts: action.payload,
+      };
+    });
+    builder.addCase(fetchAsyncNewPost.fulfilled, (state, action) => {
+      return {
+        ...state,
+        posts: [...state.posts, action.payload],
+      };
+    });
+    builder.addCase(fetchAsyncGetComments.fulfilled, (state, action) => {
+      return {
+        ...state,
+        comments: action.payload,
+      };
+    });
+    builder.addCase(fetchAsyncPostComment.fulfilled, (state, action) => {
+      return {
+        ...state,
+        comments: [...state.comments, action.payload],
+      };
+    });
+    builder.addCase(fetchAsyncPatchLiked.fulfilled, (state, action) => {
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post.id === action.payload.id ? action.payload : post
+        ),
+      };
+    });
+  },
+});
+
+// dispatchで使えるようにエクスポート
+export const {
+  fetchPostStart,
+  fetchPostEnd,
+  setOpenNewPost,
+  resetOpenNewPost,
+} = postSlice.actions;
+
+// useSelectorを使用して外部からアクセスできるようにする
+export const selectIsLoadingPost = (state: RootState) =>
+  state.post.isLoadingPost;
+export const selectOpenNewPost = (state: RootState) => state.post.openNewPost;
+export const selectPosts = (state: RootState) => state.post.posts;
+export const selectComments = (state: RootState) => state.post.comments;
+
+export default postSlice.reducer;
